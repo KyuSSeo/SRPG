fdb5a9ddcc2bc064d89fd09a51ce4a6d3facd6bc
KyuSSeo <kyuri2665@gmail.com>
2025-06-01 14:46:53 +0900

    [Feat] 기존 SelectUnitState 기능 ExploreState으로 옮김

--- Assets/Scripts/Common/Controller/BattleController.cs
+++ Assets/Scripts/Common/Controller/BattleController.cs
@@ -5,21 +5,32 @@ using UnityEngine;
 //  상태 기반으로 전투 상황 관리로 StateMachine을 이용
 public class BattleController : StateMachine
 {
+    #region Public
+    //  카메라
     public CameraRig cameraRig;
+    //  맵 정보
     public Board board;
     public LevelData levelData;
+    
+    // 선택 타일 표시
     public Transform tileSelectionIndicator;
+    //  선택 타일 정보
     public Point pos;
     
     //  맵에 영웅을 인스턴트화
     public GameObject heroPrefab;
+
+    //  선택 유닛과 타일
     public Unit currentUnit;
     public Tile currentTile { get { return board.GetTile(pos); } }
 
+    // 능력 선택 메뉴 UI
     public AbilityMenuPanelController abilityMenuPanelController;
+    //  턴 정보
     public Turn turn = new Turn();
+    //  맵에 전투중인 유닛들
     public List<Unit> units = new List<Unit>();
-
+    #endregion
 
     private void Start()
     {
--- Assets/Scripts/Common/Controller/States/SelectUnitState.cs
+++ Assets/Scripts/Common/Controller/States/SelectUnitState.cs
@@ -21,22 +21,4 @@ public class SelectUnitState : BattleState
         yield return null;
         owner.ChangeState<CommandSelectionState>();
     }
-
-    //  TODO : 지우기
-    //  미친새끼 집중력이 무슨 30분을 넘기질 못하냐 진짜 시발 병신새끼인가.
-    /*
-    protected override void OnMove(object sender, InfoEventArgs<Point> e)
-    {
-        SelectTile(e.info + pos);
-    }
-    protected override void OnFire(object sender, InfoEventArgs<int> e)
-    {
-        GameObject content = owner.currentTile.contents;
-        if (content != null)
-        {
-            owner.currentUnit = content.GetComponent<Unit>();
-            owner.ChangeState<MoveTargetState>();
-        }
-    }
-    */
 }
