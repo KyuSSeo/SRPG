74667c75d83215e70bc370213a4543233ca2a92c
KyuSSeo <kyuri2665@gmail.com>
2025-06-02 11:22:50 +0900

    [Feat] Exp

---
+++ Assets/Scripts/Common/Controller/ExperienceManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//  경험치를 분배하는 기능을 가진 스크립트
+public class ExperienceManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
