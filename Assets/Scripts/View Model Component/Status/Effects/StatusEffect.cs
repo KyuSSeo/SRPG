c392699ea6128fc3cebb6adb591e1a0cf2e6bf01
KyuSSeo <kyuri2665@gmail.com>
2025-06-04 03:28:36 +0900

    [Feat] 빈 추상 클래스 생성

---
+++ Assets/Scripts/View Model Component/Status/Effects/StatusEffect.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 빈 스크립트를 상속받으며 어떤 역할을 하는지 구분,
+/// 비슷한 역할을 하는 코드가 다량 발생 시, 빈 코드에 추가하는 등의 행위가 가능
+/// </summary>
+public abstract class StatusEffect : MonoBehaviour
+{
+
+}
